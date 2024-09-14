cd3e05c01be6c0864b1058ff30fbca5b20f6f670
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-14 19:43:03 +0530

    code modified

--- script.js
+++ script.js
@@ -89,3 +89,8 @@ randBtn.addEventListener("click", () => {
   currValue = Math.floor(Math.random() * reviews.length);
   display(currValue);
 });
+
+randBtn.addEventListener("click", () => {
+  currValue = Math.floor(Math.random() * reviews.length);
+  display(currValue);
+});
